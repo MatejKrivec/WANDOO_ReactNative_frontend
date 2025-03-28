9024185e944c37c77d3e056d6efd57e8d673e0eb
MatejKrivec <matej.krivec@gmail.com>
2025-03-28 12:28:05 +0100

    Working eas android build

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
-import '../Wandoo/assets/styles/leaflet.css';
 
 const App: React.FC = () => {
   return <AppNavigator />;
